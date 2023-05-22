11d6bd891bd6c818911825f75c188ad23f6650f9
Sigrun May <sigrun.may@gmail.com>
2023-05-22 17:17:33 +0200

    Serial training only

--- single_node_experiments_grid_batch.sh
+++ single_node_experiments_grid_batch.sh
@@ -2,8 +2,8 @@
 # This software is distributed under the terms of the MIT license
 # which is available at https://opensource.org/licenses/MIT
 
-#python experiments/experiment_serial_single_node.py
-#OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
+python experiments/experiment_serial_single_node.py
+OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
 #
 #python experiments/experiment_parallel_hpo_single_node.py
 #OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
@@ -11,5 +11,5 @@
 #python experiments/experiment_parallel_training_single_node.py
 #python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
 
-python experiments/experiment_parallel_training_gpu.py
-python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
+#python experiments/experiment_parallel_training_gpu.py
+#python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
