ccbfb2825f8c806d1b30091f017e2970aa137f13
Sigrun May <sigrun.may@gmail.com>
2023-05-23 06:07:00 +0200

    Add copyright

--- experiments_grid_batch.sh
+++ experiments_grid_batch.sh
@@ -1,3 +1,7 @@
+# Copyright (c) 2023 Sigrun May, Ostfalia Hochschule für angewandte Wissenschaften
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
 python experiments/experiment_serial_single_node.py
 OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
 
