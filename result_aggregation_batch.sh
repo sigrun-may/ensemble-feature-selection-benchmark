a1dcb1cc25abb2bf19e0e032ae552d5029cdf99e
Sigrun May <sigrun.may@gmail.com>
2023-05-24 15:59:22 +0200

    Aggregate results

---
+++ result_aggregation_batch.sh
@@ -0,0 +1,6 @@
+# Copyright (c) 2023 Sigrun May, Ostfalia Hochschule für angewandte Wissenschaften
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+python experiments/settings_data_aggregation.py
+python experiments/aggregate_results.py
