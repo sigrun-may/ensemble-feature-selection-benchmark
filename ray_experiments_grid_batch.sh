1fb39eba6353f1b3924a1b65cae7b071146dd0fc
Sigrun May <sigrun.may@gmail.com>
2023-05-26 14:56:15 +0200

    Activate parallel methods, inner and outer cv

--- ray_experiments_grid_batch.sh
+++ ray_experiments_grid_batch.sh
@@ -5,19 +5,19 @@
 # start ray cluster in conda environment
 # ray start --head and specify resources
 
-python experiments/experiment_parallel_hpo_ray.py
-OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
+#python experiments/experiment_parallel_hpo_ray.py
+#OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
 #
 #rm -r /home/sigrun/PycharmProjects/ensemble-feature-selection-benchmark/_objective*
 
-#python experiments/experiment_parallel_feature_selection_methods_ray.py
-#OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
-#
-#python experiments/experiment_parallel_inner_cv_ray.py
-#OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
-#
-#python experiments/experiment_parallel_outer_cv_ray.py
-#OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
+python experiments/experiment_parallel_feature_selection_methods_ray.py
+OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
+
+python experiments/experiment_parallel_inner_cv_ray.py
+OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
+
+python experiments/experiment_parallel_outer_cv_ray.py
+OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
 
 #python experiments/experiment_parallel_features_reverse_ray.py
 #OMP_NUM_THREADS=1 python experiments/main_benchmark.py 2>&1 | sed '/.*LightGBM].*/d'
