ae78983393a4fd2433d24849f659dfc2f31f34a0
Sigrun May <sigrun.may@gmail.com>
2023-05-26 10:45:49 +0200

    Set ConcurrencyLimiter(max_concurrent=36)

--- ensemble_feature_selection_benchmark/feature_selection_ray/embedded_feature_selection_hpo_ray.py
+++ ensemble_feature_selection_benchmark/feature_selection_ray/embedded_feature_selection_hpo_ray.py
@@ -10,6 +10,7 @@ import numpy as np
 import ray
 from ray import air, tune
 from ray.air.config import RunConfig
+from ray.tune.search import ConcurrencyLimiter
 from ray.tune.search.optuna import OptunaSearch
 from optuna.samplers import TPESampler
 
@@ -44,6 +45,7 @@ def get_results(
 
     tpe_sampler = TPESampler()
     optuna_search = OptunaSearch(sampler=tpe_sampler, mode=direction, metric="score")
+    optuna_search = ConcurrencyLimiter(optuna_search, max_concurrent=36)
 
     if "lasso" in selection_method.__name__:
         param_space = {"alpha": tune.qloguniform(0.001, 1, 0.001), "random_state": 42}
