1c02b5e856fe878ad9a48abb0848694c31b8f283
Daria <d.kober@ostfalia.de>
2023-05-26 15:36:59 +0200

    added key for number_of_measurements to benchmark_dict

--- ensemble_feature_selection_benchmark/power_measurement.py
+++ ensemble_feature_selection_benchmark/power_measurement.py
@@ -41,6 +41,7 @@ def _get_actual_node_power_usage(node_power_usages_dict: dict):
                     node_power_usages_dict[f"{node_id}_peg"] += float(
                         node["@actualPEGPowerUsage"]
                     )
+            node_power_usages_dict["number_of_measurements"] += 1
             return node_power_usages_dict
         else:
             node_power_usages_dict["error"] += 1
@@ -99,7 +100,8 @@ def initialize_benchmark_dict():
     for node_id in settings["nodes"]["baseBoardIds"]:
         benchmark_dict[f"node{node_id}"] = 0
         benchmark_dict[f"node{node_id}_peg"] = 0
-        benchmark_dict["error"] = 0
+    benchmark_dict["error"] = 0
+    benchmark_dict["number_of_measurements"] = 0
     benchmark_dict["time"] = datetime.now()
     assert type(benchmark_dict["time"]) == datetime
     return benchmark_dict
